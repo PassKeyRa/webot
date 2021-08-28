1c15bb597336143c3db01da198f8f726428e44cc
ox7a59 <ox7a59@gmail.com>
2021-08-28 20:38:18 +0300

    added outcome queue and added logger + try/catch.

--- app/aws_sqs_adapter.py
+++ app/aws_sqs_adapter.py
@@ -2,36 +2,67 @@ from json import loads
 from os import getenv
 from boto3.session import Session
 from dotenv import load_dotenv
+import logging
+
+logger = logging.getLogger(__name__)
 
 def loadEnv():
     load_dotenv()
     aws_access_key_id = getenv('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = getenv('AWS_SECRET_ACCESS_KEY')
     region_name = getenv('REGION_NAME')
-    queue_name = getenv('QUEUE_NAME')
-    return aws_access_key_id, aws_secret_access_key, region_name, queue_name
+    queue_name_in = getenv('QUEUE_NAME_IN')
+    queue_name_out = getenv('QUEUE_NAME_OUT')
+    return aws_access_key_id, aws_secret_access_key, region_name, queue_name_in, queue_name_out
 
 class AwsSqsAdapter:
 
     def __init__(self, aws_access_key_id, aws_secret_access_key, region_name):
-        self.session = Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
-        self.sqs = self.session.resource('sqs')
+        try:
+            self.session = Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+            self.sqs = self.session.resource('sqs')
+        except Exception as e:
+            logger.exception("Some error occured: {}".format(e))
+
+
+    def QueueCreate(self, queue_name):
+        try:
+            self.queue = self.sqs.create_queue(QueueName=queue_name)
+        except Exception as e:
+            logger.exception("Some error occured: {}".format(e))
 
     def QueueConnect(self, queue_name):
-        self.queue = self.sqs.get_queue_by_name(QueueName=queue_name)
+        try:
+            self.queue = self.sqs.get_queue_by_name(QueueName=queue_name)
+        except Exception as e:
+            logger.exception("Some error occured: {}".format(e))
 
     def SendMessage(self, message):
-        self.queue.send_message(MessageBody=message)
-
+        try:
+            self.queue.send_message(MessageBody=message)
+        except Exception as e:
+            logger.exception("Some error occured: {}".format(e))
 
     def ReceiveMessages(self):
-        temp = self.queue.receive_messages()
-        for message in range(len(temp)):
-            print(loads(temp[message].body)['test'])
+        temp_queue = self.queue.receive_messages()
+        try:
+            for message in range(len(temp_queue)):
+                yield loads(temp_queue[message].body)
+        except Exception as e:
+            logger.exception("Some error occured: {}".format(e))
 
+    def QueueDelete(self):
+        try:
+            self.queue.delete()
+        except Exception as e:
+            logger.exception("Some error occured: {}".format(e))
+            
 if __name__ == "__main__":
-    aws_access_key_id, aws_secret_access_key, region_name, queue_name = loadEnv()
-    test_sqs = AwsSqsAdapter(aws_access_key_id, aws_secret_access_key, region_name)
-    test_sqs.QueueConnect(queue_name)
-    test_sqs.SendMessage('{"test":"1234"}')
-    test_sqs.ReceiveMessages()
+    aws_access_key_id, aws_secret_access_key, region_name, queue_name_in, queue_name_out = loadEnv()
+    incoming_sqs = AwsSqsAdapter(aws_access_key_id, aws_secret_access_key, region_name)
+    incoming_sqs.QueueCreate(queue_name_in)
+    incoming_sqs.QueueConnect(queue_name_in)
+    incoming_sqs.SendMessage('{"test":"1234"}')
+    for i in incoming_sqs.ReceiveMessages():
+        print(i)
+    incoming_sqs.QueueDelete()
