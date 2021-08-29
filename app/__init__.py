69dc55b4a304c67257d68ba841dff80527c5f332
boggda <black_sec@protonmail.com>
2021-08-29 16:38:56 +0300

    added database model

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or \
+                           'Testing_Secret_Key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+                                        'sqlite:///' + os.path.join(basedir, 'app.db')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
