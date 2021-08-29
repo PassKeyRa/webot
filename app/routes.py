ad611c088c3deffdb0c2c395370ac5e232bedcfa
boggda <black_sec@protonmail.com>
2021-08-29 18:15:26 +0300

    added queue handler

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,47 @@
 from app import app
 from app import mongo
 from flask import render_template
+import json, random, string
+
+
+def get_mes():
+    return '{}'
+
+
+def send_mes(mes):
+    pass
+
+
+def id_generator(size=32, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 def queue_handler():
-    pass
+    while True:
+        try:
+            queue_request = get_mes()
+            request = json.loads(queue_request)
+            if request['type'] == 'new_chat':
+                new_token = id_generator()
+                mongo.db.chats.insert_one({'chat_name': request['chat_name'],
+                                           'chat_token': new_token,
+                                           'activated': True,
+                                           'messages': []})
+                send_mes(json.dumps({'Status': 'OK',
+                                     'url': f'/show_chat/{new_token}',
+                                     'token': new_token}))
+
+            elif request['type'] == 'add_message':
+                mongo.db.chats.update_one({'chat_token': request['token']},
+                                          {'$push': {'messages': request['message']}})
+
+            elif request['type'] == 'deactivate_chat':
+                mongo.db.chats.update_one({'chat_token': request['token']},
+                                          {'activated': False})
+
+        except Exception as e:
+            print(f'[!] Some error: {e}')
+            continue
 
 
 @app.route('/')
