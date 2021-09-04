475a9f830dbf1995e83b890b4ec6fcb459c2223d
boggda <black_sec@protonmail.com>
2021-09-04 16:44:31 +0300

    added class ShowChat and QueueHandler

--- app/routes.py
+++ app/routes.py
@@ -1,55 +1,57 @@
 from app import app
 from app import mongo
 from flask import render_template
+from flask.views import View
 import json, random, string
 
 
-def get_mes():
-    return '{}'
+class QueueHandler:
+    @staticmethod
+    def get_mes():
+        return '{}'
+
+    @staticmethod
+    def send_mes(mes):
+        pass
+
+    @staticmethod
+    def token_generator(size=32, chars=string.ascii_lowercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    @staticmethod
+    def queue_handler():
+        while True:
+            try:
+                queue_request = QueueHandler.get_mes()
+                request = json.loads(queue_request)
+                if request['type'] == 'new_chat':
+                    new_token = QueueHandler.token_generator()
+                    mongo.db.chats.insert_one({'chat_name': request['chat_name'],
+                                               'chat_token': new_token,
+                                               'activated': True,
+                                               'messages': []})
+                    QueueHandler.send_mes(json.dumps({'Status': 'OK',
+                                         'url': f'/show_chat/{new_token}',
+                                         'token': new_token}))
+
+                elif request['type'] == 'add_messages':
+                    for mes in request['message']:
+                        mongo.db.chats.update_one({'chat_token': request['token']},
+                                                  {'$push': {'messages': mes}})
+
+                elif request['type'] == 'delete_chat':
+                    mongo.db.chats.update_one({'chat_token': request['token']},
+                                              {'activated': False})
+
+            except Exception as e:
+                print(f'[!] Some error: {e}')
+                continue
 
 
-def send_mes(mes):
-    pass
+class ShowChat(View):
+    def dispatch_request(self, token):
+        chat = mongo.db.chats.find_one_or_404({"token": token})
+        return render_template("show_chat.html", chat=chat)
 
 
-def id_generator(size=32, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-
-def queue_handler():
-    while True:
-        try:
-            queue_request = get_mes()
-            request = json.loads(queue_request)
-            if request['type'] == 'new_chat':
-                new_token = id_generator()
-                mongo.db.chats.insert_one({'chat_name': request['chat_name'],
-                                           'chat_token': new_token,
-                                           'activated': True,
-                                           'messages': []})
-                send_mes(json.dumps({'Status': 'OK',
-                                     'url': f'/show_chat/{new_token}',
-                                     'token': new_token}))
-
-            elif request['type'] == 'add_message':
-                mongo.db.chats.update_one({'chat_token': request['token']},
-                                          {'$push': {'messages': request['message']}})
-
-            elif request['type'] == 'deactivate_chat':
-                mongo.db.chats.update_one({'chat_token': request['token']},
-                                          {'activated': False})
-
-        except Exception as e:
-            print(f'[!] Some error: {e}')
-            continue
-
-
-@app.route('/')
-def index():
-    return "Hello, World!"
-
-
-@app.route('/show_chat/<token>')
-def show_chat(token):
-    chat = mongo.db.chats.find_one_or_404({"token": token})
-    return render_template("show_chat.html", chat=chat)
+app.add_url_rule('/show_chat/<token>', view_func=ShowChat.as_view('show_chat'))
