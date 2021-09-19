de71aea16251e58703d23856caba4be33c8e7b71
German Vechtomov <ox7a59@gmail.com>
2021-09-19 20:09:33 +0300

    add normal exit.

--- webot_api.py
+++ webot_api.py
@@ -1,10 +1,14 @@
 from app import app, routes
 import threading
+import os
 
 
 if __name__ == '__main__':
-    queue_thread = threading.Thread(target=routes.QueueHandler.queue_handler)
-    queue_thread.daemon = True
-    queue_thread.start()
+    try:
+        queue_thread = threading.Thread(target=routes.QueueHandler.queue_handler)
+        queue_thread.daemon = True
+        queue_thread.start()
 
-    app.run(debug=True)
+        app.run(debug=True)
+   except KeyboardInterrupt:
+        os._exit(0)
